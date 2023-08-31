e783004076af442ca19114c9b73bcebfcfda3005
IJMPLM <iwagjohnmichael@gmail.com>
2023-08-31 18:06:56 +0800

    automated stubs and headings

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
-const rows = 11;
+const rows = 25;
 const columns = 8;
+const startingTime = 4;
 
 function schedTable(rows, columns){
     const table = document.createElement('table');
@@ -25,10 +26,49 @@ function tableHeading(){
         } else {
             specificCell.textContent = dayNames[i].substring(0,3);
         }
-    }
-
-    
+    }   
 }
+
+function classifyRows(rows){
+    for (let i = 1; i < rows; i++) {
+            const cell = document.getElementById(`cell-${i}-0`);
+            cell.classList.add('timeStub');
+        }
+}
+
+function classifyHeaders(columns){
+    for (let i = 0; i < columns; i++) {
+        const cell = document.getElementById(`cell-0-${i}`);
+        cell.classList.add('heading');
+    }
+}
+
+function time(startingTime){
+    for(let i=1; i<rows; i++){
+        const specificCell = document.getElementById(`cell-${i}-0`);
+        const timeDisplay = startingTime + i -1;
+         if(timeDisplay > 12){
+            if(timeDisplay > 24){
+                specificCell.textContent = `${timeDisplay-24}:00AM`;
+            } else{
+                if (timeDisplay == 24){
+                    specificCell.textContent = `${timeDisplay-12}:00MN`
+                } else
+                specificCell.textContent = `${timeDisplay-12}:00PM`;
+            } 
+        } else {
+            if(timeDisplay == 12){
+                specificCell.textContent = `${timeDisplay}:00NN`;
+            } else
+            specificCell.textContent = `${timeDisplay}:00AM`;
+        }
+    }
+}
+
+
 
 schedTable(rows,columns);
 tableHeading();
+classifyRows(rows);
+classifyHeaders(columns);
+time(startingTime);
